8dca481075f88ce55e39a240df4b559183ab8677
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 09:52:10 +1000

    add simple upload form

--- web/index.php
+++ web/index.php
@@ -19,6 +19,12 @@ $doc_version = "1";
     </head>
     <body>
 
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+        
         <?php
         echo "test again<br>";
         echo $doc_title;

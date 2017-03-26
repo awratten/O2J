fe07f32e686ab17a2487bbe73e184bae535b0639
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 09:03:09 +1000

    Change variable name

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
-$title = "O2J"
+$doc_title = "O2J";
+/*
+ 
+*/
 ?>
 
 
@@ -7,7 +10,7 @@ $title = "O2J"
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><?php echo $title; ?></title>
+        <title><?php echo $doc_title; ?></title>
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -19,6 +22,5 @@ $title = "O2J"
         <?php
         echo "test again";
         ?>
-
 
     </body>

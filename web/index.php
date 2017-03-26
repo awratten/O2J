eb8f92603e2bbe8067bb81f318a38f1e8202c3b8
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 08:47:50 +1000

    add css basics

--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>O2J</title>
+
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    </head>
+    <body>
+
+        <?php
+        echo "test again";
+        ?>
 
 
-echo "test";
-echo "<h1>Test 2</h1>";
+    </body>

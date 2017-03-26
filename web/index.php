6a802d51f8dc4ce2bcd5d05e703acf4aee3bb1a2
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 08:50:15 +1000

    Add php variables for document title

--- web/index.php
+++ web/index.php
@@ -1,8 +1,13 @@
+<?php
+$title = "O2J"
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>O2J</title>
+        <title><?php echo $title; ?></title>
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">

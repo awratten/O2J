e950057a1bd52eb34b5b5669fb665b83bbb10344
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 12:35:11 +1000

    test without main.css

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $doc_version = "1";
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel="stylesheet" type="text/css" href="/css/main2.css">
         
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         

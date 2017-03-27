f192393da62f49797178e4b5290e3d063490db94
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 12:46:49 +1000

    reformat html

--- web/index.php
+++ web/index.php
@@ -24,48 +24,40 @@ $doc_version = "1";
 
     </head>
     <body class="bg-primary" style="overflow-x: hidden;">
-        <label class="custom-file">
-            <input type="file" id="file" class="custom-file-input">
-            <span class="custom-file-control"></span>
-        </label>
-        <div class="center-wrapper">
-            <div class="center-content">
-                <div class="row">
-                    <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                        <img style="margin-bottom: 45px;display: block;margin-left: auto;margin-right: auto;" src="http://placehold.it/200x64" alt="Logo">
-                        <section class="panel bg-white no-b">
-                            <ul class="switcher-dash-action">
-                                <li class="active"><a href="/login">Office</a></li>
-                                <li><a href="/users/sign_up">PDF</a></li>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <img style="margin-bottom: 45px;display: block;margin-left: auto;margin-right: auto;" src="http://placehold.it/200x64" alt="Logo">
+
+                    <div class="card text-center">
+                        <div class="card-header">
+                            <ul class="nav nav-tabs card-header-tabs">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#">MS Office</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">PDF</a>
+                                </li>
                             </ul>
-                            <div class="p15">
-                                <form class="new_user" id="new_user" action="/login" accept-charset="UTF-8" method="post" _lpchecked="1"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="PSGX6ZWxrNQi76c7GTTsnobdOmbfUjmG4KuzNIh8dl0gX1VuPmqfaTdx9cKrWqo7NxTGe2YPGTUXeIncqq8H+A==">
-                                    <div class="field">
-                                        <label class="custom-file">
-                                            <input type="file" id="file" class="custom-file-input">
-                                            <span class="custom-file-control"></span>
-                                        </label>
-                                    </div>
-                                    <br>
-                                    <div class="actions">
-                                        <button name="commit" type="submit" class="btn btn-primary btn-lg btn-block">Get Hash</button>
-                                    </div>
-                                </form>
-                                <div class="mt5">
-                                    <a style="color: #9B9B9B;" href="/users/password/new">Forgot your password?</a>
-                                    <a style="color: #9B9B9B;float: right" href="/users/confirmation/new">Didn't receive confirmation instructions?</a>
-                                </div>
-                            </div>
-                        </section>
-                        <p class="text-center">
-                            Copyright ©
-                            <span id="year" class="mr5"> 2017</span>
-                            <span>Selly LLC</span>
-                        </p>
+                        </div>
+                        <div class="card-block">
+                            <label class="custom-file">
+                                <input type="file" id="file" class="custom-file-input">
+                                <span class="custom-file-control"></span>
+                            </label>
+                        </div>
                     </div>
+
+                    <p class="text-center">
+                        Copyright ©
+                        <span id="year" class="mr5"> 2017</span>
+                        <span>Selly LLC</span>
+                    </p>
                 </div>
             </div>
         </div>
+
 
 
         <?php

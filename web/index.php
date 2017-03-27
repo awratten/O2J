7ef457f30d778c73cc9f435d790ea03a3a0b05b2
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 11:10:40 +1000

    add viewport scale

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@ $doc_version = "1";
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?php echo $doc_title; ?></title>
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">

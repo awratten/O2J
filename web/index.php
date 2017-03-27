ad68a4f74d62e78841c3f456531e01fb197777db
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-28 09:06:06 +1000

    add favicon

--- web/index.php
+++ web/index.php
@@ -18,6 +18,13 @@ if (isset($_GET['hash'])) {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?php echo $doc_title; ?></title>
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon//apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="favicon//favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="favicon//favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="favicon//manifest.json">
+        <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">

b78728b1358cde6209b7a89a657bc7d032bb8d88
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-28 07:46:29 +1000

    strip to basic  - display upload name

--- web/get_hash.php
+++ web/get_hash.php
@@ -1,39 +1,6 @@
 <?php
 
-$info = pathinfo($uploadfile);
-$date = (new \DateTime())->format('Y-m-d H:i:s');
-
 $uploaddir = '/var/www/html/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['upload']['name']);
-
-$filename = basename($_FILES['upload']['name']);
-$filename = preg_replace('/\s+/', '_', $filename);
-$filename = (string)$filename;
-$uploadfile = preg_replace('/\s+/', '_', $uploadfile);
-
-if (!is_file($uploadfile)) {
-    move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile);
-} else {
-	$name_split = explode(".", $filename);
-	$filename = $name_split[0]."-".time().".".$name_split[1];
-	$newname = $uploaddir . $filename;
-	$uploadfile = $newname;
-	move_uploaded_file($_FILES['upload']['tmp_name'], $newname);
-}
-
-$name_split = explode(".", $filename);
-
-exec("python /var/www/html/office2john.py $uploadfile ", $output, $status);
-
-$hash = (string)implode('',$output);
-
-if ($hash == '') {
-//header('Location: /index.php?error=1');      // On Error alert
-echo "Error Extracting Hash - No hash may be present or file type may be unsupported.";
-} else {
-echo $hash;
-//header('Location: /index.php?hash='.$hash);      // On Error alert    
-}
 
 
-
+echo $_FILES['upload']['name'];

918c085fa6965d2945d567782bf1f68d6e9f8ff5
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-28 08:08:46 +1000

    basic upload test

--- web/get_hash.php
+++ web/get_hash.php
@@ -1,5 +1,31 @@
 <?php
+
 $date = (new \DateTime())->format('Y-m-d H:i:s');
 $uploaddir = '/var/www/html/O2J/web/uploads/';
 
-$uploadfile = $uploaddir . basename($_FILES['upload']['name']);
+function Filter_FileName($fname) {
+    $a = preg_replace('/\s+/', '_', $fname);
+    $b = (string) $a;
+    return $b;
+}
+
+$filename = Filter_FileName(basename($_FILES['upload']['name']));
+
+$uploadfile = $uploaddir . $filename;
+
+
+if (!is_file($uploadfile)) {
+    move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile);
+} else {
+    $name_split = explode(".", $filename);
+    $filename = $name_split[0] . "-" . time() . "." . $name_split[1];
+    $newname = $uploaddir . $filename;
+    $uploadfile = $newname;
+    move_uploaded_file($_FILES['upload']['tmp_name'], $newname);
+}
+
+echo $filename;
+echo "<br>";
+echo $uploaddir;
+echo "<br>";
+echo $date;

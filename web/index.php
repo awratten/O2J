9700d376cdbe13fee1e57022f301f0fe6329a254
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 16:19:10 +1000

    php trigger - fix error

--- web/index.php
+++ web/index.php
@@ -22,7 +22,6 @@ $doc_version = "1";
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-
     </head>
     <body class="bg-primary" style="overflow-x: hidden; margin-top: 50px;">
 
@@ -98,9 +97,8 @@ $doc_version = "1";
             echo "$(document) . ready(function () {";
             echo "$('#HashModal') . modal('show');";
             echo "});";
+            echo "</script>";
         }
-        </script>
-
-    ?>
+        ?>
     </body>
 </html>

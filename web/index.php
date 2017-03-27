c543b69d21fbae03c225edcfa2e66fd2be7f66fb
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-27 15:04:00 +1000

    fix modal

--- web/index.php
+++ web/index.php
@@ -71,7 +71,15 @@ $doc_version = "1";
                     <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-lg">
                             <div class="modal-content">
-                                <span>...</span>
+                                <div class="modal-header">
+                                    <h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
                             </div>
                         </div>
                     </div>

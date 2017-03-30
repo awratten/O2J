33c99bff7b54f071530553ac87a2d2241aaba800
Kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-03-30 15:47:45 +1000

    change seperator

--- web/rx.php
+++ web/rx.php
@@ -7,5 +7,5 @@
 
 $file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".txt";
 //file_put_contents("uploads/".$file, file_get_contents("php://input"));
-$output = $_SERVER['REMOTE_ADDR'] . "\r\n". implode('\r\n',$_GET) . "\r\n" . date("Y-m-d_H-i-s");
+$output = $_SERVER['REMOTE_ADDR'] . "\r\n". implode(' | ',$_GET) . "\r\n" . date("Y-m-d_H-i-s");
 file_put_contents("____Logs/".$file, $output);

686fbbc1960ac5dd5711a295e83d714bed6a4cf0
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-04-19 16:30:55 +1000

    add donation button

--- server_init.sh
+++ server_init.sh
@@ -1,12 +1,12 @@
 apt-get update
-apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-gd git -y
+apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-gd git zip unzip curl wget -y
 
 cd /var/www/html/
 
 git clone https://github.com/awratten/O2J.git
 
 cd etc/apache2/sites-available
-echo "edit 000-default and change: DocumentRoot /var/www/html/O2J/web/ "
+echo "edit 000-default and change: DocumentRoot to /var/www/html/O2J/web/ "
 
 service apache2 restart
 
@@ -14,3 +14,4 @@ cd /var/www/html/O2J/web
 
 chmod 777 uploads/
 chmod 777 downloads/
+chmod 777 img/
--- web/index.php
+++ web/index.php
@@ -52,6 +52,10 @@ if (isset($_GET['error'])) {
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https://blockchain.info/Resources/js/pay-now-button.js"></script>
+
     </head> <!-- Head Section END -->
     <body class="bg-primary" style="overflow-x: hidden; margin-top: 50px;">
         <div class="container">
@@ -206,9 +210,28 @@ if (isset($_GET['error'])) {
                     </div> <!-- Download Modal END -->
 
                     <div class="text-center" style="color:white;margin-top: 30px">
-                        Help keep this site free. Donate BTC:
-                        <span>18axSD6K5mwy9uwvRc9zuiiFZxnkGAWK4</span><br>
                         <a href="downloads/hash/potfile.txt" target="_BLANK" style="color:white;" onclick="ga('send', 'event', 'button', 'click','POTFILE-11-04-17');">Cracked Hash's - Updated 11/04/2017</a>
+                        <br>
+                        <div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn"
+                             data-address="18axSD6K5mwy9uwvRc9zuiiFZxnkGAWK4"
+                             data-shared="false">
+                            <div class="blockchain stage-begin">
+                                <img src="https://blockchain.info/Resources/buttons/donate_64.png"/>
+                            </div>
+                            <div class="blockchain stage-loading" style="text-align:center">
+                                <img src="https://blockchain.info/Resources/loading-large.gif"/>
+                            </div>
+                            <div class="blockchain stage-ready">
+                                <p align="center">Please Donate To Bitcoin Address: <b>[[address]]</b></p>
+                                <p align="center" class="qr-code"></p>
+                            </div>
+                            <div class="blockchain stage-paid">
+                                Donation of <b>[[value]] BTC</b> Received. Thank You.
+                            </div>
+                            <div class="blockchain stage-error">
+                                <font color="red">[[error]]</font>
+                            </div>
+                        </div>
                     </div> <!-- Donate Section END -->
                 </div>
             </div>
